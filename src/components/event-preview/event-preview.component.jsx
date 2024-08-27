3f91f9c3fdf01670cd442d84d4b3c15e10d7db6e
OsimFavour <famos204@gmail.com>
2024-08-27 05:59:47 +0100

    Create Event Preview component

---
+++ src/components/event-preview/event-preview.component.jsx
@@ -0,0 +1,70 @@
+const EventPreview = () => {
+    return (
+        <section className="section" id="explore">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="left-content">
+                            <h2>Enrol for Esther's Women Academy here</h2>
+                            <span>
+            
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore 
+                                magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation.
+                            </span>
+                            
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore 
+                                magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation.
+                            </p>
+
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                                sed do eiusmod tempor incididunt ut labore et dolore 
+                                magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation.
+                            </p>
+                            
+                            <div className="main-border-button">
+                                <a href="products.html">Discover More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="right-content">
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="leather">
+                                        <h4>Tutorials</h4>
+                                        <span>New Courses</span>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="first-image">
+                                        <img src='https://i.ibb.co/hDFhm97/home-img.jpg' height='255px' width='255px' alt=""/>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="second-image">
+                                        <img src='https://i.ibb.co/qg6qdrF/home-img-02.jpg' height='255px' width='255px' alt=""/>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6">
+                                    <div className="types">
+                                        <h4>Best Selling Books</h4>
+                                        <span>Over 304 Books</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default EventPreview

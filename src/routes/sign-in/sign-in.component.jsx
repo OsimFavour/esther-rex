c2b5a56bb8f7c23524627dc8009da90c191da12e
OsimFavour <famos204@gmail.com>
2024-09-27 20:14:38 +0100

    Modify sign in form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -47,7 +47,7 @@ const SignIn = () => {
                 </div> */}
                 <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
 
-                    <h5 className='pb-3 text-center'>Welcome! please enter your details.</h5>
+                    <div className='pb-3 text-center'>Welcome! please enter your details.</div>
                     <form 
                         action=''
                         onSubmit={formik.handleSubmit}

9031b8a97ded4606926a29fb44b17eb19e1082b1
OsimFavour <famos204@gmail.com>
2024-10-04 11:18:30 +0100

    Use book review component

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
 import BlogPreview from "../../components/blog-preview/blog-preview.component"
-// import BookPreview from "../../components/book-preview/book-preview.component"
+import BookPreview from "../../components/book-preview/book-preview.component"
 import EventPreview from "../../components/event-preview/event-preview.component"
 import LandingPage from "../../components/landing-page/landing-page.component"
 
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <>
             <LandingPage />
-            {/* <BookPreview /> */}
+            <BookPreview />
             <BlogPreview />
             <EventPreview />
         </>

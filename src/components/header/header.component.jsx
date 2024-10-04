b58edcdfbc2339536a0383d33a5fbc8473cf0fcb
OsimFavour <famos204@gmail.com>
2024-10-04 11:19:16 +0100

    Modify header links

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -45,7 +45,7 @@ const Header = () => {
                             <ul className={`nav ${isMenuOpen ? 'show' : ''}`} onClick={handleLinkClick}>
                                 <li className="scroll-to-section"><Link to="/" className="active">Home</Link></li>
                                 <li className="scroll-to-section"><Link to="/blog">Blog</Link></li>
-                                <li className="scroll-to-section"><Link to="">Ministry</Link></li>
+                                <li className="scroll-to-section"><Link to="/ewa">EWA</Link></li>
                                 <li className="scroll-to-section"><Link to="/contact">Contact</Link></li>
                                 <li className="scroll-to-section"><Link to="/sign-in">Login</Link></li>
                                 
